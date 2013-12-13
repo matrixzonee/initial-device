050c28dbd22cc234f33af6770f266331eb4d435e
matrixzonee <kunalsethi@gmail.com>
2013-12-13 20:37:14 +0530

    first publish

---
+++ extract-files.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+export DEVICE=d2i747
+export VENDOR=samsung
+./../d2-common/extract-files.sh $@
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo omni_d2att-userdebug
+add_lunch_combo full_d2att-userdebug
+
